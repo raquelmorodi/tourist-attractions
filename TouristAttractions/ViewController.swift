98f71a829825dcb5fa4f3cdab8cbc7eafbb6fba9
Raquel <raquel6531sa@gmail.com>
2026-03-09 18:45:03 +0200

    Initial Commit

---
+++ TouristAttractions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TouristAttractions
+//
+//  Created by Raquel Morodi on 2026/03/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
